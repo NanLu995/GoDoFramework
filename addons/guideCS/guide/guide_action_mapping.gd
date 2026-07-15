da14c063447e1b417973b90911b58024ee522180
NanLu995 <lancetti928@qq.com>
2026-07-15 17:21:44 +0800

    chore(deps): 引入 GUIDE 与 Phantom Camera 插件

---
+++ addons/guideCS/guide/guide_action_mapping.gd
@@ -0,0 +1,26 @@
+@icon("res://addons/guideCS/guide/guide_internal.svg")
+@tool
+## An action to input mapping
+class_name GUIDEActionMapping
+extends Resource
+
+## The action to be mapped
+@export var action:GUIDEAction:
+	set(value):
+		if value == action:
+			return
+		action = value
+		emit_changed()
+
+## A set of input mappings that can trigger the action
+@export var input_mappings:Array[GUIDEInputMapping] = []:
+	set(value):
+		if value == input_mappings:
+			return
+		input_mappings = value
+		emit_changed()		
+
+
+## Remote call of input_mappings set() for use in the C# wrapper.
+func remote_set_input_mappings(maps:Array) -> void:
+	input_mappings.assign(maps)
